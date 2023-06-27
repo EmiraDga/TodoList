cf308e5603ce2385c6a5aee5996913530c91ede4
EmiraDga <emiradga888@gmail.com>
2023-06-27 12:53:55 +0100

    remove the java project and initilize the project with angular

--- src/main/java/tn/project/todolist/repository/TodoItemRepo.java
+++
@@ -1,7 +0,0 @@
-package tn.project.todolist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.project.todolist.entities.TodoItem;
-
-public interface TodoItemRepo  extends JpaRepository<TodoItem,Long> {
-}
